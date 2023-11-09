98bb36ba2d087a947144e800eff123e856c6480b
Alfred Rosario <alfredsnk@gmail.com>
2023-11-09 11:03:34 -0400

    schedule page updates, paypal implementation and aesthetic general changes

--- index.py
+++ index.py
@@ -11,6 +11,8 @@ from bson import json_util
 import json
 import os
 from dotenv import load_dotenv
+from werkzeug.security import generate_password_hash, check_password_hash
+import paypalrestsdk
 app = Flask(__name__)
 load_dotenv()
 mail_username = os.getenv('MAIL_USERNAME')
@@ -32,6 +34,11 @@ collectionReptile = db['Reptile']
 collectionBird = db['Birds']
 collectionAmphibian = db['Amphibians']
 collectionMammal = db['Mammals']
+
+paypalrestsdk.configure({
+  "mode": "sandbox", 
+  "client_id": "AbjKwdLo3BCZGYEwe6RV2AOv8YfZHQp_mw22SIjZ3BgERdAlYirlRD4_If5a7Ig8H3-NezemnblSpOPI",
+  "client_secret": "EOxAlD-DUhFnD9QOZnhtCxQhEjwWijOcOe5tx5gpZj4t1uB3UGFB3mOyN3Z1bIIB2SOtQK-UL2eTTSg2" })
 
 class CustomHTMLCalendar(calendar.HTMLCalendar):
     def __init__(self, year, month, day):
@@ -150,52 +157,154 @@ def sendEmail():
 @app.route("/scheduleEntry")
 def schedulePage():
     return render_template('schedulePage.html')
+
+@app.route("/adminRegistrationPage")
+def adminRegistrationPage():
+    return render_template('adminRegistrationPage.html')
+
+@app.route("/adminLoginPage")
+def adminloginPage():
+    return render_template('adminLoginPage.html')
+
+@app.route("/adminPage")
+def adminPage():
+    return render_template('adminPage.html')
+
+@app.route("/userRegistration")
+def userRegistrationPage():
+    return render_template('userRegistrationPage.html')
+
+@app.route("/loginPage")
+def loginPage():
+    return render_template('userLoginPage.html')
+
+@app.route("/paymentProcessing")
+def paymentProcessing():
+    return render_template('paymentProcessing.html')
+
+adminCollection = db['savedAdminData']
+
 class User:
-    def __init__(self, userName, email, password):
+    def __init__(self, username, firstName, lastName, email, password, role='user'):
+        self.username = username
+        self.firstName = firstName
+        self.lastName = lastName
         self.email = email
-        self.password = password
-        self.userName = userName
+        self.password = generate_password_hash(password)
+        self.role = role
 
     def to_json(self):
         return {
-            "userName": self.userName,
+            "username": self.username,
+            "firstName": self.firstName,
+            "lastName": self.lastName,
+            "email": self.email,
+            "password": self.password,
+            "role": self.role
+        }
+    
+adminCollection = db['savedAdminData']
+class AdminUser:
+    def __init__(self, username, firstName, lastName, email, password):
+        self.username = username
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.password = generate_password_hash(password)
+        
+    def toJson(self):
+        return {
+            "username": self.username,
+            "firstName": self.firstName,
+            "lastName": self.lastName,
             "email": self.email,
             "password": self.password
         }
 
+@app.route('/signupAdmin', methods=['POST'])
+def adminSignup():
+    data = request.get_json()
+    admin_username = data.get('adminUsername')
+    admin_first_name = data.get('adminFirstName')
+    admin_last_name = data.get('adminLastName')
+    admin_email = data.get('adminEmail')
+    admin_confirm_email = data.get('adminConfirmEmail')
+    admin_password = data.get('adminPassword')
+    admin_confirm_password = data.get('adminConfirmPassword')
+    
+    if admin_email != admin_confirm_email:
+        return jsonify({'status': 'fail', 'message': 'Emails do not match.'})
+    if admin_password != admin_confirm_password:
+        return jsonify({'status': 'fail', 'message': 'Passwords do not match.'})
+
+    existing_admin = db.users.find_one({'email': admin_email})
+    if existing_admin is None:
+        new_admin = User(admin_username, admin_first_name, admin_last_name, admin_email, admin_password, 'admin')
+        db.users.insert_one(new_admin.to_json())
+        session['adminUsername'] = admin_username
+        session['role'] = 'admin'
+        return jsonify({'status': 'success', 'message': 'Admin successfully registered.'})
+    else:
+        return jsonify({'status': 'fail', 'message': 'Admin already exists.'})
 
 @app.route('/signup', methods=['POST'])
 def signup():
-    email = request.form['email']
-    password = request.form['password']
-    userName = request.form['userName']
-    existingUser = db.users.find_one({'email': email})
+    data = request.get_json()  
+    username = data.get('username')
+    first_name = data.get('firstName')
+    last_name = data.get('lastName')
+    email = data.get('email')
+    password = data.get('password')
+    confirm_password = data.get('confirmPassword')
+    
+    if email != data.get('confirmEmail'):
+        return jsonify({'status': 'fail', 'message': 'Emails do not match.'})
+    if password != confirm_password:
+        return jsonify({'status': 'fail', 'message': 'Passwords do not match.'})
 
-    if existingUser is None:
-        session['username'] = userName
-        newUser = User(userName, email, password)
-        db.users.insert_one(newUser.to_json())
-        return jsonify({'status': 'success', 'message': 'Successfully registered user.'})
+    existing_user = db.users.find_one({'email': email})
+    if existing_user is None:
+ 
+        new_user = User(username, first_name, last_name, email, password)
+        db.users.insert_one(new_user.to_json())
+
+        return jsonify({'status': 'success', 'message': 'User successfully registered.', 'redirect': url_for('loginPage')})
     else:
         return jsonify({'status': 'fail', 'message': 'User already exists.'})
 
 
-@app.route('/login', methods=['POST'])
-def login():
-    email = request.form['email']
-    password = request.form['password']
-    user = db.users.find_one({'email': email})
+@app.route('/adminLogin', methods=['POST'])
+def adminLogin():
+    data = request.get_json() 
+    adminEmail = data.get('adminEmail')
+    adminPassword = data.get('adminPassword')
+    adminUser = db.users.find_one({'email': adminEmail})
 
-    if user and 'password' in user and user['password'] == password:
-        username = user.get('userName', None)
-        if username is not None:
-            session['username'] = username
-            return jsonify({'status': 'success', 'message': 'Successfully logged in.', 'redirect': url_for('home')})
+    if adminUser and check_password_hash(adminUser['password'], adminPassword):
+        adminUsername = adminUser.get('username', None)
+        if adminUsername:
+            session['adminUsername'] = adminUsername
+            return jsonify({'status': 'success', 'message': 'Successfully logged in.', 'redirect': url_for('adminPage')})
         else:
             return jsonify({'status': 'fail', 'message': 'Username does not exist.'})
     else:
         return jsonify({'status': 'fail', 'message': 'Invalid email or password.'})
 
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    email = data.get('email')
+    password = data.get('password')
+    
+    user = db.users.find_one({'email': email})
+    
+    if user and check_password_hash(user['password'], password):
+        session['username'] = user['username']
+        session['role'] = user.get('role', 'user')  
+        redirect_url = 'adminPage' if session['role'] == 'admin' else 'home'
+        return jsonify({'status': 'success', 'message': 'Successfully logged in.', 'redirect': url_for(redirect_url)})
+    else:
+        return jsonify({'status': 'fail', 'message': 'Invalid email or password.'})
 
 @app.route('/logout')
 def logout():
@@ -221,7 +330,15 @@ def addSavedDate():
             "visitors": visitors,  
             "guidedTour": guided_tour  
         }
+        
         datesCollection.insert_one(newDateAdded)
+        session['reservation_info'] = {
+            "date": date,
+            "name": name,
+            "lastName": lastName,
+            "visitors": visitors,
+            "guidedTour": guided_tour  
+        }
         return jsonify({'status': 'success', 'message': 'Date added successfully.'})
     else:
         return jsonify({'status': 'fail', 'message': 'Usuario no autenticado.'})
@@ -237,7 +354,6 @@ def getDates():
             last_name = date.get('lastName', '')
             title = f"{name} {last_name}".strip() if name or last_name else "Zoo Visit"
             event_id = str(date.get('_id', ''))
-
             events.append({
                 'id': event_id,
                 'title': title,
@@ -252,6 +368,49 @@ def getDates():
     else:
         return jsonify([])
 
+@app.route('/payment', methods=['POST'])
+def payment():
+
+    payment = paypalrestsdk.Payment({
+        "intent": "sale",
+        "payer": {
+            "payment_method": "paypal"},
+        "redirect_urls": {
+            "return_url": "http://localhost:5000/payment/execute",
+            "cancel_url": "http://localhost:5000/"},
+        "transactions": [{
+            "item_list": {
+                "items": [{
+                    "name": "testitem",
+                    "sku": "12345",
+                    "price": "1000.00",
+                    "currency": "USD",
+                    "quantity": 1}]},
+            "amount": {
+                "total": "1000.00",
+                "currency": "USD"},
+            "description": "This is the payment transaction description."}]})
+
+    if payment.create():
+        print('Payment success!')
+    else:
+        print(payment.error)
+
+    return jsonify({'paymentID' : payment.id})
+
+@app.route('/execute', methods=['POST'])
+def execute():
+    success = False
+
+    payment = paypalrestsdk.Payment.find(request.form['paymentID'])
+
+    if payment.execute({'payer_id' : request.form['payerID']}):
+        print('Execute success!')
+        success = True
+    else:
+        print(payment.error)
+
+    return jsonify({'success' : success})
 
 googleCredentialsPath = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
 creds = service_account.Credentials.from_service_account_file(
